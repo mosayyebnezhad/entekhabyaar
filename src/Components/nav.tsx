80a5de3ebb7f970ec8884bfc8e9f89294b5602c2
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-02-03 15:03:46 +0330

    a

--- src/Components/nav.tsx
+++ src/Components/nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
         <nav className="w-5/6 mx-auto my-4 flex justify-between items-center">
             <div className="flex items-center gap-2">
                 <img src="./par.svg" alt="" />
-                <h1 className="text-2xl md:text-3xl font-black">استادبان</h1>
+                <h1 className="text-2xl md:text-3xl font-black">انتخاب‌یار</h1>
             </div>
             <div className="flex gap-2">
                 
